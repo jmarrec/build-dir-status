42ae833bd0e27d89f3934b4feeb177ab4e2aec6e
Julien Marrec <julien.marrec@gmail.com>
2022-07-29 12:09:09 +0200

    Explicitly test TimeUtilities

---
+++ test/TimeUtilities_GTest.cpp
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+
+#include "../src/TimeUtilities.hpp"
+
+#include <algorithm>
+#include <string>
+#include <string_view>
+
+using namespace build_dir_status;
+
+TEST(TimeUtilitiesTest, convert_str_to_time_t) {
+
+  std::string time_str = "2022-07-28T15:45";
+  EXPECT_NO_THROW(convert_str_to_time_t(time_str));
+
+  time_str = "blablabla";
+  EXPECT_ANY_THROW(convert_str_to_time_t(time_str));
+}
+
+TEST(TimeUtilitiesTest, elapsed_time) {
+
+  std::string start_time_str = "2022-07-28T15:45";
+  std::string end_time_str = "2022-07-28T16:47";
+
+  std::string elapsed = elapsed_time(start_time_str, end_time_str);
+  EXPECT_EQ("01:02", elapsed_time(start_time_str, end_time_str));
+
+  end_time_str = "2022-07-29T14:45";
+  EXPECT_EQ("23:00", elapsed_time(start_time_str, end_time_str));
+
+  end_time_str = "2022-07-29T16:45";
+  EXPECT_EQ("1 day", elapsed_time(start_time_str, end_time_str));
+
+  end_time_str = "2022-07-30T14:45";
+  EXPECT_EQ("1 day", elapsed_time(start_time_str, end_time_str));
+
+  end_time_str = "2022-07-30T16:45";
+  EXPECT_EQ("2 days", elapsed_time(start_time_str, end_time_str));
+
+  end_time_str = "2022-07-31T16:45";
+  EXPECT_EQ("3 days", elapsed_time(start_time_str, end_time_str));
+
+  end_time_str = "2022-08-01T16:45";
+  EXPECT_EQ("4 days", elapsed_time(start_time_str, end_time_str));
+}
